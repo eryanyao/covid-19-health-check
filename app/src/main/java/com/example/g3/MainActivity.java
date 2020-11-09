91f4b5275b5cfbc3a5157bb0da76dfb8e4fd2738
y-t-1234-y-t <ytlow1234@gmail.com>
2020-11-09 23:29:43 +0800

    add covid activity through API

--- app/src/main/java/com/example/g3/MainActivity.java
+++ app/src/main/java/com/example/g3/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.g3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCovidAct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCovidAct = findViewById(R.id.buttonCovidAct);
+        btnCovidAct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), CovidActivity.class));
+            }
+        });
     }
 }
