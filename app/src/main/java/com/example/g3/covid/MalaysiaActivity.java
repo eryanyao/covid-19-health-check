3870978a7338954dd6b4785d188e98dc5012d47b
y-t-1234-y-t <ytlow1234@gmail.com>
2020-11-14 00:19:15 +0800

    add malaysia covid activity

--- app/src/main/java/com/example/g3/covid/MalaysiaActivity.java
+++ app/src/main/java/com/example/g3/covid/MalaysiaActivity.java
@@ -8,8 +8,6 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -21,12 +19,15 @@ import com.leo.simplearcloader.SimpleArcLoader;
 
 import org.eazegraph.lib.charts.PieChart;
 import org.eazegraph.lib.models.PieModel;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class CovidActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    TextView tvCases, tvRecovered, tvCritical, tvActive, tvTodayCases, tvTotalDeaths, tvTodayDeaths, tvAffectedCountries;
+public class MalaysiaActivity extends AppCompatActivity {
+
+    TextView tvCases, tvRecovered, tvCritical, tvActive, tvTodayCases, tvTotalDeaths, tvTodayDeaths;
     SimpleArcLoader simpleArcLoader;
     ScrollView scrollView;
     PieChart pieChart;
@@ -34,33 +35,31 @@ public class CovidActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.covid_act_main);
+        setContentView(R.layout.malaysia_covid_data);
 
-        getSupportActionBar().setTitle("Covid-19 Tracker");
+        getSupportActionBar().setTitle("Malaysia Covid-19 Status");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
 
-        tvCases = findViewById(R.id.tvCases);
-        tvRecovered = findViewById(R.id.tvRecovered);
-        tvCritical = findViewById(R.id.tvCritical);
-        tvActive = findViewById(R.id.tvActive);
-        tvTodayCases = findViewById(R.id.tvTodayCases);
-        tvTotalDeaths = findViewById(R.id.tvTotalDeaths);
-        tvTodayDeaths = findViewById(R.id.tvTodayDeaths);
-        tvAffectedCountries = findViewById(R.id.tvAffectedCountries);
+        tvCases = findViewById(R.id.tvCasesMalaysia);
+        tvRecovered = findViewById(R.id.tvRecoveredMalaysia);
+        tvCritical = findViewById(R.id.tvCriticalMalaysia);
+        tvActive = findViewById(R.id.tvActiveMalaysia);
+        tvTodayCases = findViewById(R.id.tvTodayCasesMalaysia);
+        tvTotalDeaths = findViewById(R.id.tvTotalDeathsMalaysia);
+        tvTodayDeaths = findViewById(R.id.tvTodayDeathsMalaysia);
 
-        simpleArcLoader = findViewById(R.id.loader);
-        scrollView = findViewById(R.id.scrollStats);
-        pieChart = findViewById(R.id.piechart);
+        simpleArcLoader = findViewById(R.id.loaderMalaysia);
+        scrollView = findViewById(R.id.scrollStatsMalaysia);
+        pieChart = findViewById(R.id.piechartMalaysia);
 
         fetchData();
     }
 
     private void fetchData() {
 
-        //get global's covid-19 data
-        String url = "https://disease.sh/v3/covid-19/all";
+        String url = "https://disease.sh/v3/covid-19/countries";
 
         simpleArcLoader.start();
 
@@ -70,16 +69,34 @@ public class CovidActivity extends AppCompatActivity {
                     public void onResponse(String response) {
 
                         try {
-                            JSONObject jsonObject = new JSONObject(response.toString());
+                            JSONArray jsonArray = new JSONArray(response);
+                            for (int i = 0; i < jsonArray.length(); i++) {
 
-                            tvCases.setText(jsonObject.getString("cases"));
-                            tvRecovered.setText(jsonObject.getString("recovered"));
-                            tvCritical.setText(jsonObject.getString("critical"));
-                            tvActive.setText(jsonObject.getString("active"));
-                            tvTodayCases.setText(jsonObject.getString("todayCases"));
-                            tvTotalDeaths.setText(jsonObject.getString("deaths"));
-                            tvTodayDeaths.setText(jsonObject.getString("todayDeaths"));
-                            tvAffectedCountries.setText(jsonObject.getString("affectedCountries"));
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                                String countryName = jsonObject.getString("country");
+
+                                if(countryName.equalsIgnoreCase("Malaysia")) {
+                                    String cases = jsonObject.getString("cases");
+                                    String todayCases = jsonObject.getString("todayCases");
+                                    String deaths = jsonObject.getString("deaths");
+                                    String todayDeaths = jsonObject.getString("todayDeaths");
+                                    String recovered = jsonObject.getString("recovered");
+                                    String active = jsonObject.getString("active");
+                                    String critical = jsonObject.getString("critical");
+
+                                    //get malaysia's flag icon
+//                                    JSONObject object = jsonObject.getJSONObject("countryInfo");
+//                                    String flagUrl = object.getString("flag");
+                                    tvCases.setText(cases);
+                                    tvRecovered.setText(recovered);
+                                    tvCritical.setText(critical);
+                                    tvActive.setText(active);
+                                    tvTodayCases.setText(todayCases);
+                                    tvTotalDeaths.setText(deaths);
+                                    tvTodayDeaths.setText(todayDeaths);
+                                }
+                            }
 
                             pieChart.addPieSlice(new PieModel("Cases", Integer.parseInt(tvCases.getText().toString()), Color.parseColor("#FFA726")));
                             pieChart.addPieSlice(new PieModel("Recoverd", Integer.parseInt(tvRecovered.getText().toString()), Color.parseColor("#66BB6A")));
@@ -105,19 +122,11 @@ public class CovidActivity extends AppCompatActivity {
                 simpleArcLoader.stop();
                 simpleArcLoader.setVisibility(View.GONE);
                 scrollView.setVisibility(View.VISIBLE);
-                Toast.makeText(CovidActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(MalaysiaActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(request);
-    }
-
-    public void goTrackCountries(View view) {
-        startActivity(new Intent(getApplicationContext(), AffectedCountries.class));
-    }
-
-    public void goMalaysiaCovidActivity(View view) {
-        startActivity(new Intent(getApplicationContext(), MalaysiaActivity.class));
     }
 }
