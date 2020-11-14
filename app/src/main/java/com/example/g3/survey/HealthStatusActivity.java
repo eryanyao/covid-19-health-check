ce32d6d75ff665928457e08ab52f1b726584093e
y-t-1234-y-t <ytlow1234@gmail.com>
2020-11-14 18:04:42 +0800

    add health activity interface

--- app/src/main/java/com/example/g3/survey/HealthStatusActivity.java
+++ app/src/main/java/com/example/g3/survey/HealthStatusActivity.java
@@ -1,27 +1,7 @@
 package com.example.g3.survey;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ScrollView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.example.g3.R;
-import com.example.g3.covid.MalaysiaActivity;
-import com.leo.simplearcloader.SimpleArcLoader;
-
-import org.eazegraph.lib.charts.PieChart;
-import org.eazegraph.lib.models.PieModel;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -30,8 +10,7 @@ public class HealthStatusActivity  extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.malaysia_covid_data);
-
+        setContentView(R.layout.health_survey_activity_main);
 
     }
 }
